753b7e64703e46ffe985b5b0782bf6b0f560fd6b
DanteHalfeye <deadcoolest@gmail.com>
2024-02-20 23:26:13 -0500

    Botones menu

--- Assets/scripts/boton jugar.cs
+++ Assets/scripts/boton jugar.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    [Tooltip("the (exact) name of the Scene to go play ")]
+    [SerializeField] string playLevelName = "";
+    [Tooltip("the (exact) name of the Scene to see the instructions ")]
+    [SerializeField] string instructionsLevelName = "";
+    [Tooltip("the (exact) name of the Scene to see the credits ")]
+    [SerializeField] string creditsLevelName = "";
     public void OnClickJugar()
     {
-        MMSceneLoadingManager.LoadScene("Nivel1");
-
-
+        MMSceneLoadingManager.LoadScene(playLevelName);
     }
+    public void OnClickInstrucciones()
+    {
+        MMSceneLoadingManager.LoadScene(instructionsLevelName);
+    }
+    public void OnClickCreditos()
+    {
+        MMSceneLoadingManager.LoadScene(creditsLevelName);
+    }
+    public void OnClickSalir()
+    {
+        Application.Quit(); 
+    }
+
 }
