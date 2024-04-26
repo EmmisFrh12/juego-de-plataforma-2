6805de18d33049657684e572292a72fb04953b1f
Emma <emmisfrh12@gmail.com>
2024-04-26 12:29:27 -0500

    Terminar nivel 3

---
+++ Assets/scripts/ganó.cs
@@ -0,0 +1,14 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gano : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ONGano()
+    {
+        MMSceneLoadingManager.LoadScene("gano");
+        //Debug.Log("abrir puerta");
+    }
+}
